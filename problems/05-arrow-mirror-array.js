1e30d25a0702de6e3dedbd2b247e4649b4f9a5ae
TheSaviourEking <saviour.eking@outlook.com>
2023-09-01 03:15:13 +0100

    05-arrow-mirror-array.js

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,6 +10,13 @@ mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a
 
 const mirrorArray = (array) => {
   // Your code here
+  const mirrored = [...array];
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    mirrored.push(array[i]);
+  }
+
+  return mirrored;
 };
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
