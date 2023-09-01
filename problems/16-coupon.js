90eda69ed1ec145e42c2aa832de8e2e1758646ad
TheSaviourEking <saviour.eking@outlook.com>
2023-09-01 03:19:41 +0100

    16-coupon.js

--- problems/16-coupon.js
+++ problems/16-coupon.js
@@ -13,11 +13,17 @@ let twentyPercent = coupon(0.2);
 console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 
 
-***********************************************************************/
+**********************************************************************/
 function coupon(discount) {
   // Your code here
+  return (array => {
+    return array.map(el => {
+      return el -= el * discount;
+    })
+  })
 }
-
+let tenPercent = coupon(0.1);
+console.log(tenPercent([10, 20, 30])); // [ 9, 18, 27 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = coupon;
