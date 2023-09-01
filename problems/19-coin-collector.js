b5f5aebbe447046130c2bc0a1221df14f37d6121
TheSaviourEking <saviour.eking@outlook.com>
2023-09-01 03:21:11 +0100

    add 19-coin-collector.js

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -26,8 +26,30 @@ Example 3:
 ***********************************************************************/
 function coinCollector(numCoins) {
   // Your code here
-}
+  let result = [];
 
+  return (coin) => {
+    result.push(coin);
+    numCoins--
+
+    if (numCoins > 0) {
+      return coinCollector(numCoins)
+    } else {
+      return result
+    }
+  }
+}
+let oneCoin = coinCollector(1); // returns a function
+// console.log(oneCoin(10)); // prints [10]
+
+let twoCoins = coinCollector(2); // returns a function
+twoCoins(25); // returns a function
+console.log(twoCoins(10)); // prints [25, 10]
+
+let threeCoins = coinCollector(3); // returns a function
+threeCoins(25); // returns a function
+threeCoins(5); // returns a function
+console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = coinCollector;
