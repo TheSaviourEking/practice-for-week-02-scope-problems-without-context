4c8a28b90c88c386b36767dc119677fb4d30dc55
TheSaviourEking <saviour.eking@outlook.com>
2023-09-01 03:15:58 +0100

    07-set-time-out.js

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -20,9 +20,15 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 function funcTimer(time, func) {
   // Your code here
+  return setTimeout(func, time);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+function partyFunc () {
+  console.log("Party time!")
+}
+
+funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 try {
   module.exports = funcTimer;
